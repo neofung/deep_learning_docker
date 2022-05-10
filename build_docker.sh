6c2ab76e5fd8f3efeca88d2cc3bbba7f05440641
Neo Fung <neosfung@gmail.com>
2022-05-10 22:11:15 +0800

    first commit

---
+++ build_docker.sh
@@ -0,0 +1,20 @@
+cd $(dirname $0)/
+set -xe
+VERSION=0.0.1
+
+UBUNTU_VERSION=20.04
+CONDA_VERSION=4.7.12.1
+OS_TYPE=x86_64
+PYTHON_VERSION=3.8.11
+TENSORFLOW_VERSION=2.5.0
+PYTORCH_VERSION=1.5.0
+
+sed 's#UBUNTU_VERSION#'${UBUNTU_VERSION}'#g' ./Dockerfile.cpu.template |
+sed 's#CONDA_VERSION#'${CONDA_VERSION}'#g'	|
+sed 's#OS_TYPE#'${OS_TYPE}'#g'			|
+sed 's#PYTHON_VERSION#'${PYTHON_VERSION}'#g'   	|
+sed 's#TENSORFLOW_VERSION#'${TENSORFLOW_VERSION}'#g'   |
+sed 's#PYTORCH_VERSION#'${PYTORCH_VERSION}'#g'   > Dockerfile.cpu
+
+docker build ${EXTRA_ARGS} -t neofung/deep_learning_cpu:latest \
+    -t neofung/deep_learning:${VERSION}-py_${PYTHON_VERSION}-tf_${TENSORFLOW_VERSION}-torch_${PYTORCH_VERSION}-cpu -f Dockerfile.cpu  .
